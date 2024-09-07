6ef6bba9f68d8675e9db81a3e5cc057c24f6d8c9
ernestisibor9 <60038336+ernestisibor9@users.noreply.github.com>
2024-09-07 12:39:20 +0100

    Update Login.jsx

--- frontend/real_estate/src/pages/Login/Login.jsx
+++ frontend/real_estate/src/pages/Login/Login.jsx
@@ -35,7 +35,7 @@ function Login() {
       } else {
         try {
           const response = await axios.post(
-            "http://localhost:5000/api/user/login",
+            "https://westminster-real-estate-backend.onrender.com/api/user/login",
             user
           );
           // console.log(response);
