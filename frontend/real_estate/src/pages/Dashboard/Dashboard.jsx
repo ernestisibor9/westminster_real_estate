e96a7691387fe2890cb71f8d3c4b67ce9ae99bff
ernestisibor9 <60038336+ernestisibor9@users.noreply.github.com>
2024-09-07 12:30:46 +0100

    Update Dashboard.jsx

--- frontend/real_estate/src/pages/Dashboard/Dashboard.jsx
+++ frontend/real_estate/src/pages/Dashboard/Dashboard.jsx
@@ -21,7 +21,7 @@ function Dashboard() {
     try {
       const token = JSON.parse(localStorage.getItem("user"));
       const response = await axios.get(
-        "http://localhost:5000/api/user/getloggedinuser",
+        "https://westminster-real-estate-backend.onrender.com/api/user/getloggedinuser",
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -74,7 +74,7 @@ function Dashboard() {
     useEffect(() => {
       const fetchDocumentCount = async () => {
         try {
-          const response = await axios.get('http://localhost:5000/api/user/total-users');
+          const response = await axios.get('https://westminster-real-estate-backend.onrender.com/api/user/total-users');
           console.log(response.data);
           
           setCountUser(response.data.message);
@@ -109,7 +109,7 @@ function Dashboard() {
     useEffect(() => {
       const fetchDocumentCount = async () => {
         try {
-          const response = await axios.get('http://localhost:5000/api/property/count-pending-properties');
+          const response = await axios.get('https://westminster-real-estate-backend.onrender.com/api/property/count-pending-properties');
           console.log(response.data);
           
           setCountPending(response.data.count);
