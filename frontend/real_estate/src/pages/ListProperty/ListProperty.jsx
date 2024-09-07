3f88f5f01fcf3e67fd3b5b10714d25acce180b05
ernestisibor9 <60038336+ernestisibor9@users.noreply.github.com>
2024-09-07 12:38:30 +0100

    Update ListProperty.jsx

--- frontend/real_estate/src/pages/ListProperty/ListProperty.jsx
+++ frontend/real_estate/src/pages/ListProperty/ListProperty.jsx
@@ -21,7 +21,7 @@ function ListProperty() {
     try {
       const token = JSON.parse(localStorage.getItem("user"));
       const response = await axios.get(
-        "http://localhost:5000/api/user/getloggedinuser",
+        "https://westminster-real-estate-backend.onrender.com/api/user/getloggedinuser",
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -85,7 +85,7 @@ function ListProperty() {
     setError("");
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/property/search-for",
+        "https://westminster-real-estate-backend.onrender.com/api/property/search-for",
         { propertyFor }
       );
       setFeaturedProperties(response.data);
@@ -111,7 +111,7 @@ function ListProperty() {
     setError("");
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/property/search-types",
+        "https://westminster-real-estate-backend.onrender.com/api/property/search-types",
         { propertyTypes }
       );
       setFeaturedProperties(response.data);
@@ -129,7 +129,7 @@ function ListProperty() {
   // Get All Featured Poperties
   const getAllFeaturedProperties = async () => {
     const response = await axios.get(
-      "http://localhost:5000/api/property/get-all-properties"
+      "https://westminster-real-estate-backend.onrender.com/api/property/get-all-properties"
     );
     setFeaturedProperties(response.data);
     console.log(response.data);
